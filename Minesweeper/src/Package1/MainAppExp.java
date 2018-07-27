129779a74e018565a03b80892a95470dc27c12bd
Revanth Reddy Pamulapati <revanthreddy194@gmail.com>
2018-07-27 09:31:20 +0530

    Add files via upload

---
+++ Minesweeper/src/Package1/MainAppExp.java
@@ -0,0 +1,14 @@
+    package Package1;
+
+
+public class MainAppExp {
+	
+	public static void main(String[] args) throws Exception {
+		
+		InitialiseExp ie = new InitialiseExp();
+		
+		ie.mainPlay();
+		
+	}
+
+}
